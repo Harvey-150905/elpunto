0b6ebd607ec9bf2e83993c1bb734915845132844
Harvey-150905 <151656194+Harvey-150905@users.noreply.github.com>
2025-10-01 15:19:16 -0500

    Update urls.py

--- elpunto/urls.py
+++ elpunto/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("task.urls")),  # 👈 /api/health/
+    path("api/", include("task.urls")),
 ]
 
 if settings.DEBUG:
